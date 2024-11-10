011a75bec1c2391f84c21b23fa602b4f4502284d
Rivva Chahwala <133695331+rivv0@users.noreply.github.com>
2024-11-10 13:26:31 +0530

    Update DBConnection.java

--- utils/DBConnection.java
+++ utils/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/doctor_management";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "#Jayarajj16*";
+    private static final String PASSWORD = "passwd";
 
     public static Connection getConnection() {
         Connection connection = null;
